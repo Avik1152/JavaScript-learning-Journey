bc48b080751c87fa1c91b3c4e8835eecafd2ae6c
Avik1152 <avik1152000@gmail.com>
2026-06-07 09:11:28 +0530

    Initial JavaScript practice setup

---
+++ 01-veriablePractice/normalProblems/normalVariableProblems.js
@@ -0,0 +1,41 @@
+// get User given value in the variable using prompt()
+// let num = parseInt(prompt("Enter first Number"));
+// let num2 = parseInt(prompt("Enter Seconed Number"));
+
+// console.log(num + num2);
+
+let hello = "Hello World!",
+  Message;
+
+Message = hello;
+
+alert(Message);
+// alert(hello);
+
+const myBirthDay = "11.05.2000";
+myBirthDay = "11.05.2000";
+
+alert(`My Birth Day is ${myBirthDay}`);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+// ...when we need to pick a color
+// When reading the code, COLOR_ORANGE is much more meaningful than #FF7F00.
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+let admin, name;
+name = "John";
+
+admin = name;
+
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+alert(ourPlanetName);
+alert(currentUserName);
