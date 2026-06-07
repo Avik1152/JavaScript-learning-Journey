59233a69cef63bb45b6f69ba8ba56bf1352a84fc
Avik1152 <avik1152000@gmail.com>
2026-06-07 15:25:04 +0530

    Real-Life Problem 2 — Student Report Card

--- 01-veriablePractice/realLifeProblems/realLifeVariableProblems.js
+++ 01-veriablePractice/realLifeProblems/realLifeVariableProblems.js
@@ -5,7 +5,7 @@
 // Item 3: Sugar → ₹60
 
 // Store each price in a variable
-let Rice_Price = 120;
+/*let Rice_Price = 120;
 let Oil_Price = 95;
 let Sugar_Price = 60;
 // Calculate total bill
@@ -23,4 +23,35 @@ let ammount_pay_after_Discount =
 // "Amount to Pay: ₹247.5"
 console.log(`Total Bill: ₹${total_Price_Of_All_Items}`);
 console.log(`Discount: ₹${discount_On_Total_Price}`);
-console.log(`Amount to Pay: ₹${ammount_pay_after_Discount}`);
+console.log(`Amount to Pay: ₹${ammount_pay_after_Discount}`);*/
+
+// Real-Life Problem 2 — Student Report Card
+
+// Ask user 3 subject marks using prompt()
+// Subject 1: Math
+// Subject 2: Science
+// Subject 3: English
+let mathObtainedMarks = parseInt(prompt("Write the Obtained marks of Math"));
+let scienceObtainedMarks = parseInt(
+  prompt("Write the Obtained marks of Science"),
+);
+let englishObtainedMarks = parseInt(
+  prompt("Write the Obtained marks of English"),
+);
+// Calculate total and average
+let totalObtainedMarksOfAllSubject =
+  mathObtainedMarks + scienceObtainedMarks + englishObtainedMarks;
+let averageOfTotalObtainedMarks = totalObtainedMarksOfAllSubject / 3;
+// Print:
+// "Total Marks: [total] / 300"
+console.log(`Total Obtained Marks: ${totalObtainedMarksOfAllSubject} / 300`);
+// "Average: [average]"
+console.log(`Average of total obtained marks: ${averageOfTotalObtainedMarks}`);
+// "Result: Pass" (if average >= 40) or "Result: Fail"
+if (averageOfTotalObtainedMarks >= 40) {
+  let resultPass = "Pass";
+  console.log(`Result: ${resultPass}`);
+} else {
+  let resultFail = "Fail";
+  console.log(`Result: ${resultFail}`);
+}
