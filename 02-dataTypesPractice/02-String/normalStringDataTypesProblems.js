4b40d2887a670638d5c725c5dada3ecde0e358bb
Avik1152 <avik1152000@gmail.com>
2026-06-09 05:19:43 +0530

    String Normal Problem 3 - Accessing characters and use of lenght

--- 02-dataTypesPractice/02-String/normalStringDataTypesProblems.js
+++ 02-dataTypesPractice/02-String/normalStringDataTypesProblems.js
@@ -13,8 +13,25 @@ let list_Of_SuperHeroes = `* Spider Man
 * Super Man
 * Men In Black`;
 
-console.log(list_Of_SuperHeroes);
+// console.log(list_Of_SuperHeroes);
 
 let list_Of_Cartoon = " Chota Bheem \n Motu Patlu \n Ben 10";
 
-console.log(list_Of_Cartoon);
+// console.log(list_Of_Cartoon);
+
+let str1 = `Hello
+World`;
+
+let str2 = "Hello\nWorld";
+
+// console.log(str1 === str2); //true*
+
+//String Normal Problem 3 - Accessing characters and use of lenght
+
+let accessing_Charecter = "Hello";
+
+console.log(accessing_Charecter[0]); //H
+console.log(accessing_Charecter[-2]); //undefined
+console.log(accessing_Charecter[accessing_Charecter.length - 2]); //l
+console.log(accessing_Charecter.at(-1)); //o
+console.log(accessing_Charecter.at(-2)); //l
