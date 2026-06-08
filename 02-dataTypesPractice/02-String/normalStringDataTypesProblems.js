04897b463c7a91ba69bc8794eccfce5ce153aa2a
Avik1152 <avik1152000@gmail.com>
2026-06-09 05:04:19 +0530

    String Normal Problem 2 - multiple lines and special character

--- 02-dataTypesPractice/02-String/normalStringDataTypesProblems.js
+++ 02-dataTypesPractice/02-String/normalStringDataTypesProblems.js
@@ -4,4 +4,17 @@
 function sum(group_a_Score, group_b_Score) {
   return group_a_Score + group_b_Score;
 }
-console.log(`The Sum of group: A and group: B is: ${sum(15, 10)}`); //embed expressions ${…}
+// console.log(`The Sum of group: A and group: B is: ${sum(15, 10)}`); //embed expressions ${…}
+
+// String Normal Problem 2 - multiple lines and special character
+let list_Of_SuperHeroes = `* Spider Man
+* Bat Man
+* Ant Man
+* Super Man
+* Men In Black`;
+
+console.log(list_Of_SuperHeroes);
+
+let list_Of_Cartoon = " Chota Bheem \n Motu Patlu \n Ben 10";
+
+console.log(list_Of_Cartoon);
