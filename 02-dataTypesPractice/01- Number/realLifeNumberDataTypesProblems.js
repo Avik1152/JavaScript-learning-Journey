9f37c781ed03a04387b9205c352593afd01a86fa
Avik1152 <avik1152000@gmail.com>
2026-06-08 11:02:11 +0530

    Number Real-Life Problem 2 — Temperature Converter

--- 02-dataTypesPractice/01- Number/realLifeNumberDataTypesProblems.js
+++ 02-dataTypesPractice/01- Number/realLifeNumberDataTypesProblems.js
@@ -3,7 +3,7 @@
 // A person takes a loan of ₹50,000
 // Interest rate is 10% per year
 // Loan duration is 2 years
-let userLoanTotalAmmaunt = 65000;
+/*let userLoanTotalAmmaunt = 65000;
 let interestRatePerYear = 10;
 let durationOfLoanYear = 2;
 // Calculate:
@@ -23,4 +23,19 @@ console.log(`Total Ammount Of Interset ₹${totalAmmountOfInterest}`);
 // "Total Payable: ₹[amount]"
 console.log(`Total Payble Loan Ammount ₹${totalPaybleLoanAmmount}`);
 // "Monthly EMI: ₹[amount]"
-console.log(`Total Monthly EMI: ₹${totalMonthlyEMI}`);
+console.log(`Total Monthly EMI: ₹${totalMonthlyEMI}`);*/
+
+// 🌡️ Number Real-Life Problem 2 — Temperature Converter
+
+// Ask user to enter temperature in Celsius using prompt()
+let userPlaceTemperature = parseInt(prompt("Enter Tempereture in Celcious"));
+// Convert it to Fahrenheit
+// Formula: (celsius * 9/5) + 32
+let toFarenheit = (userPlaceTemperature * 9) / 5 + 32;
+// Print:
+// "Temperature in Celsius: [value]°C"
+console.log(`Temperature in Celsius: ${userPlaceTemperature}°C`);
+// "Temperature in Fahrenheit: [value]°F"
+console.log(`Temperature in Farenheit: ${toFarenheit}°F`);
+
+// Test with: 0°C → 32°F, 100°C → 212°F
