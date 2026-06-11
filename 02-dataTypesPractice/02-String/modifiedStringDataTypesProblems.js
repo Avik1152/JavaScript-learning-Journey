c2dd7c5f06302acbe1cd4b016681c6aa742408bc
Avik1152 <avik1152000@gmail.com>
2026-06-11 10:37:21 +0530

    String Modified Problems 1 - String Inspector

---
+++ 02-dataTypesPractice/02-String/modifiedStringDataTypesProblems.js
@@ -0,0 +1,17 @@
+// String Modified Problems 1 - String Inspector
+
+let sentence = "JavaScript is the language of the web.";
+
+// Find and print:
+// 1) Total length of sentence
+console.log(sentence.length); //  38
+// 2) First character
+console.log(sentence[0]); //  J
+// 3) Last character using .at()
+console.log(sentence.at(-1)); //  .
+// 4) Index of word "language"
+console.log(sentence.indexOf("language")); //  18
+// 5) Does it include "web"? (true/false)
+console.log(sentence.includes("web")); //  true
+// 6) Character at index 11
+console.log(sentence.charAt(11)); //  i
